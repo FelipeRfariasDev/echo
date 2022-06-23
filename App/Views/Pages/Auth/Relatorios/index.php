811dfc04865f029496ac95356124dba70de1e894
Felipe R Farias <terceiro-ffarias@icatuseguros.com.br>
2022-06-23 20:42:40 -0300

    relatório campos obrigatórios

--- App/Views/Pages/Auth/Relatorios/index.php
+++ App/Views/Pages/Auth/Relatorios/index.php
@@ -8,16 +8,16 @@ include($_SERVER["DOCUMENT_ROOT"] . "/App/Views/Pages/Auth/Element/nav-menu-aute
     <div class="inputs">
         <div class="input-row">
             <label for="">Tipo</label>
-            <select name="tipo_relatorio">
+            <select name="tipo_relatorio" required>
                 <option value="">Selecione</option>
                 <option value="km_rodado_veiculo">KM Rodado Veiculo</option>
                 <option value="consumo_veiculo_funcionario">Consumo de Veiculo por Funcionário</option>
                 <option value="consumo_km_funcionario">Consumo KM por Funcionário</option>
             </select>
             <label for="">Data Inicio</label>
-            <input autocomplete="off" name="data_inicio" type="date">
+            <input autocomplete="off" name="data_inicio" type="date" required>
             <label for="">Data Fim</label>
-            <input autocomplete="off" name="data_fim" type="date">
+            <input autocomplete="off" name="data_fim" type="date" required>
         </div>
     </div>
     <span class="inputs"><input type="submit" value="Gerar" class="btncrud" /></span>
