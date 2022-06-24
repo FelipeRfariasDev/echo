04e11319662d0a979edd1ca26281b6ee4cbedd78
Felipe R Farias <terceiro-ffarias@icatuseguros.com.br>
2022-06-24 12:04:13 -0300

    Em relatórios a data inicio e fim já vem como sugestão os ultimos 30 dias, sendo que a data pode ser alterada.

--- App/Views/Pages/Auth/Relatorios/index.php
+++ App/Views/Pages/Auth/Relatorios/index.php
@@ -4,6 +4,7 @@ include($_SERVER["DOCUMENT_ROOT"] . "/App/Views/Pages/Auth/Element/nav-menu-aute
 ?>
     <h1 class="title" style="float: left;margin-left: 42%">Relatórios</h1>
 <br><br><br>
+
 <form action="/<?php echo $vars["nameController"];?>/index" method="POST">
     <div class="inputs">
         <div class="input-row">
@@ -14,10 +15,18 @@ include($_SERVER["DOCUMENT_ROOT"] . "/App/Views/Pages/Auth/Element/nav-menu-aute
                 <option value="consumo_veiculo_funcionario">Consumo de Veiculo por Funcionário</option>
                 <option value="consumo_km_funcionario">Consumo KM por Funcionário</option>
             </select>
+            <?php
+            $data_inicio = new DateTime("-31 days");
+            $data_inicio_formatada =  $data_inicio->format('Y-m-d');
+            ?>
             <label for="">Data Inicio</label>
-            <input autocomplete="off" name="data_inicio" type="date" required>
+            <input autocomplete="off" name="data_inicio" value="<?php echo $data_inicio_formatada ?>" type="date" required>
             <label for="">Data Fim</label>
-            <input autocomplete="off" name="data_fim" type="date" required>
+            <?php
+            $data_fim = new DateTime();
+            $data_fim_formatada =  $data_fim->format('Y-m-d');
+            ?>
+            <input autocomplete="off" name="data_fim" type="date" value="<?php echo $data_fim_formatada ?>" required>
         </div>
     </div>
     <span class="inputs"><input type="submit" value="Gerar" class="btncrud" /></span>
